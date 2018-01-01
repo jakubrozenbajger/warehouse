0b7facf9922b4aca82ee835a1f253c7761dd4f5f
Jakub Rozenbajger <thequetran@gmail.com>
2018-01-01 19:36:40 +0100

    update SaleDocumentBuilderFactory

--- src/main/kotlin/cf/jrozen/po/warehouse/service/SaleDocumentBuilderFactory.kt
+++ src/main/kotlin/cf/jrozen/po/warehouse/service/SaleDocumentBuilderFactory.kt
@@ -1,13 +1,17 @@
 package cf.jrozen.po.warehouse.service
 
 import cf.jrozen.po.warehouse.domain.SaleDocumentType
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Component
 
 @Component
 class SaleDocumentBuilderFactory(
-        val saleDocumentService: SaleDocumentService,
         val companyService: CompanyService
 ) {
+    @Autowired
+    @Lazy
+    lateinit var saleDocumentService: SaleDocumentService
 
     fun getBuilder(saleDocumentRequest: SaleDocumentRequest): AbstractSaleDocumentBuilder {
         return when (saleDocumentRequest.type) {
