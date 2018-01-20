2144c9abb5497e57c3668694612a025c27fce001
Jakub Drapala <studentwizpwr@gmail.com>
2018-01-21 00:43:32 +0100

    testy4

--- src/main/kotlin/cf/jrozen/po/warehouse/controller/SaleDocumentController.kt
+++ src/main/kotlin/cf/jrozen/po/warehouse/controller/SaleDocumentController.kt
@@ -1,8 +1,12 @@
 package cf.jrozen.po.warehouse.controller
 
+import cf.jrozen.po.warehouse.domain.Order
 import cf.jrozen.po.warehouse.service.SaleDocumentService
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * [SaleDocumentController] is responsible for managing with [SaleDocument] entity
+ */
 @RestController
 class SaleDocumentController(
         val saleDocumentService: SaleDocumentService
