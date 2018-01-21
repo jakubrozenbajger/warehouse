729ef6aea26ce62ca67a08e0de52aa84196ac66d
Jakub Rozenbajger <thequetran@gmail.com>
2018-01-21 16:26:05 +0100

    fix controller tests

--- src/test/kotlin/cf/jrozen/po/warehouse/controller/CustomerControllerTest.kt
+++ src/test/kotlin/cf/jrozen/po/warehouse/controller/CustomerControllerTest.kt
@@ -1,20 +1,27 @@
 package cf.jrozen.po.warehouse.controller
 
+import cf.jrozen.po.warehouse.testutils.randomCustomer
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 
 class CustomerControllerTest : AbstractControllerTest() {
+
     @Autowired
     lateinit var customerController: CustomerController
 
     val baseUrl = "/customers"
 
     @Test
-    fun shouldCreateNewUser() {
+    fun shouldResponseWith200() {
         mockMvc.perform(get(baseUrl))
                 .andExpect { ok() }
-//                .andExpect()
+    }
+
+    @Test
+    fun shouldCreateNewUser() {
+        mockMvc.perform(post(baseUrl, randomCustomer()))
+                .andExpect { ok() }
     }
 }
