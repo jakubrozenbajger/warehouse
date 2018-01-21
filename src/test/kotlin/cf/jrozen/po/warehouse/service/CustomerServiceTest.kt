6f8cb21c72b6cb15510959fa794fb6febec52dc5
Jakub Drapala <studentwizpwr@gmail.com>
2018-01-21 15:56:59 +0100

    testy11

--- src/test/kotlin/cf/jrozen/po/warehouse/service/CustomerServiceTest.kt
+++ src/test/kotlin/cf/jrozen/po/warehouse/service/CustomerServiceTest.kt
@@ -1,29 +1,46 @@
 package cf.jrozen.po.warehouse.service
 
+import cf.jrozen.po.warehouse.common.CustomerDeletionException
+import cf.jrozen.po.warehouse.controller.dto.CustomerDto
 import cf.jrozen.po.warehouse.controller.dto.CustomerMapper
+import cf.jrozen.po.warehouse.domain.Address
+import cf.jrozen.po.warehouse.domain.Customer
+import cf.jrozen.po.warehouse.domain.Dealer
 import cf.jrozen.po.warehouse.repository.AbstractDatabaseTest
 import cf.jrozen.po.warehouse.repository.CustomerRepository
+import cf.jrozen.po.warehouse.repository.OrderRepository
+import cf.jrozen.po.warehouse.repository.SaleDocumentRepository
 import cf.jrozen.po.warehouse.testutils.randomCustomer
+import cf.jrozen.po.warehouse.testutils.randomDealer
 import cf.jrozen.po.warehouse.testutils.randomOrderPosition
 import cf.jrozen.po.warehouse.testutils.randomReceipt
+import org.hibernate.criterion.Order
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDateTime
+import javax.persistence.EntityNotFoundException
 
 class CustomerServiceTest : AbstractDatabaseTest() {
 
     @Autowired
     lateinit var customerRepository: CustomerRepository
 
-    lateinit var customerService : CustomerService
+    @Autowired
+    lateinit var orderRepository: OrderRepository
+
+    lateinit var orderService: OrderService
+    lateinit var customerService: CustomerService
+
     var customer = randomCustomer()
     var id = customer.customerUuid
 
     @Before
-    fun bef(){
+    fun bef() {
         customerService = CustomerService(customerRepository, Mockito.mock(CustomerMapper::class.java))
+        orderService = OrderService(orderRepository, Mockito.mock(SaleDocumentService::class.java), Mockito.mock(FinanceProcessingStrategy::class.java), Mockito.mock(SaleDocumentRepository::class.java))
     }
 
     @Test
@@ -43,15 +60,38 @@ class CustomerServiceTest : AbstractDatabaseTest() {
     fun canDeleteTheSameTwice() {
 
         customerRepository.save(customer)
+        customerService.deleteCustomer(id)
 
-        customer.canBeDeleted()
+        assertFalse(customerRepository.exists(customer.customerUuid))
 
-        if (customer.canBeDeleted()) {
+        try {
             customerService.deleteCustomer(id)
-            customerService.deleteCustomer(id)
+        } catch (e: Exception) {
+            assert(e.cause is EntityNotFoundException)
         }
+
     }
 
+    @Test
+    fun cantDeleteCustomerWithCommitment() {
+        val custom = randomCustomer()
+        val order = cf.jrozen.po.warehouse.domain.Order(
+                "id",
+                randomDealer(),
+                "descript",
+                custom
+        )
 
+
+        customerRepository.save(custom)
+        orderRepository.save(order)
+
+        try {
+            customerService.deleteCustomer(custom.customerUuid)
+        } catch (e: Exception) {
+            assert(e.cause is CustomerDeletionException)
+        }
+
+    }
 
 }
